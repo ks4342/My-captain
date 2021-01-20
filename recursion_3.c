cbbdbe513cbd8f1968d2ce605ab3345217eb391b
ks4342 <77532747+ks4342@users.noreply.github.com>
2021-01-21 00:17:42 +0530

    Update recursion_3.c

--- recursion_3.c
+++ recursion_3.c
@@ -1,16 +1,19 @@
 
-int sum_of_digit(int n) 
-{ 
-    if (n == 0) 
-       return 0; 
-    return (n % 10 + sum_of_digit(n / 10)); 
-} 
-  
+#include<stdio.h>
+int sum_of_digit(int n)
+{
+    if (n == 0)
+       return 0;
+    return (n % 10 + sum_of_digit(n / 10));
+}
 
-int main() 
-{ 
-    int num = 12345; 
-    int result = sum_of_digit(num); 
-    printf("Sum of digits in %d is %d\n", num, result); 
-    return 0; 
-} 
+
+int main()
+{
+    printf("Input number: \t");
+    int num ;
+    scanf("%d" ,&num);
+    int result = sum_of_digit(num);
+    printf("Sum of digits: %d " , result);
+    return 0;
+}
